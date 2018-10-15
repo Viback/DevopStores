72a96baed55a8b22a235902a4c357ec0f41e0262
Anna-Mari Lamminen <annis@AnnaMarminenMBP.home>
2018-10-15 23:09:54 +0300

    Eight commit

--- testApp/d22t8omvseiqtsContext.cs
+++ testApp/d22t8omvseiqtsContext.cs
@@ -26,7 +26,7 @@ namespace testApp
                 var cstring = Environment.GetEnvironmentVariable("DATABASE_URL"); 
                 var connstr = cstring + "SslMode = Require; trust server certificate = true";
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
+                optionsBuilder.UseNpgsql(connstr);
             }
         }
 
