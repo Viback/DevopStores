2116e27dbb2f1f6278a8bd306b21082203af9978
Anna-Mari Lamminen <annis@AnnaMarminenMBP.home>
2018-10-22 22:20:46 +0300

    Commit #37

--- testApp/Inventory.cs
+++ testApp/Inventory.cs
@@ -5,7 +5,7 @@ namespace testApp
 {
     public partial class Inventory
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public int StoreId { get; set; }
         public int ProdId { get; set; }
         public int Qty { get; set; }
